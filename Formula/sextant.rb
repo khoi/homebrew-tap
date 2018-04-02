0e1fbad2367bfe61113dd9c0b1764c1f39b72765
goreleaserbot <goreleaser@carlosbecker.com>
2018-04-02 18:33:57 +0700

    Brew formula update for sextant version 1.3.0

--- Formula/sextant.rb
+++ Formula/sextant.rb
@@ -1,9 +1,9 @@
 class Sextant < Formula
   desc ""
   homepage ""
-  url "https://github.com/khoiracle/sextant/releases/download/1.2.0/sextant_1.2.0_darwin_amd64.tar.gz"
-  version "1.2.0"
-  sha256 "97e4b5b6b2314457a8eb34b8d40ba906a55bab3c0696a9b10eead83d85af884e"
+  url "https://github.com/khoiracle/sextant/releases/download/1.3.0/sextant_1.3.0_darwin_amd64.tar.gz"
+  version "1.3.0"
+  sha256 "e32c3a69e8b8d9a9d64db47a624bcd421ccccc8df4eaaf0624230c7f1903b841"
 
   def install
     bin.install "sextant"
